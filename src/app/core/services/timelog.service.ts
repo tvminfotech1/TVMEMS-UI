16cf64a5b50e8d7f2609f3aaa607493dac0f9e97
arokiajenis-lab <arokiajenis@gmail.com>
2025-12-18 12:04:49 +0530

    media query changes

--- src/app/core/services/timelog.service.ts
+++ src/app/core/services/timelog.service.ts
@@ -61,7 +61,7 @@ export interface Holiday {
 @Injectable({ providedIn: "root" })
 export class TimelogService {
   private readonly userApiUrl = `${BASE_URL}/user/timesheet`;
-  private readonly adminAllUrl = `${BASE_URL}/user/timesheet/allTimsheet`;
+  private readonly adminAllUrl = `${BASE_URL}/user/timesheet/allTimesheet`;
 
   constructor(private http: HttpClient) {}
   private extractArrayFromResponse(res: any): any[] {
